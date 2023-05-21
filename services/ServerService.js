f736ea4e31596acbffd198df1de82c44a5cddae4
Youba.I <mevshypocrisie@yahoo.com>
2023-05-21 21:09:37 +0200

    ✨(feature): update homeScreen logic

---
+++ services/ServerService.js
@@ -0,0 +1,25 @@
+// import React from "react";
+// import { View, Text } from "react-native/types";
+import axios from "axios";
+
+
+export const ServerService = {
+
+    testConnection: async (baseUrl, port) => {
+        try{
+            const response = await axios.get(`http://${baseUrl}:${port}/`);
+            if (response.status === 200)
+            {
+                return true;
+            } else {
+                return false;
+            }
+        } catch(error) {
+            throw new Error('Connection Failed');
+        }
+    }
+
+    
+
+    
+};
