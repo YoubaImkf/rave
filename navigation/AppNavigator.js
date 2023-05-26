2b3890cf4f390c71d591b3a8c2d386b2ff1b7e7a
Youba.I <mevshypocrisie@yahoo.com>
2023-05-26 17:37:16 +0200

    🎨(icon): add icon on tab nav

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -1,22 +1,43 @@
-import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import HomeScreen from '../screens/HomeScreen';
 import RecordScreen from '../screens/RecordScreen';
 import RaveScreen from '../screens/RaveScreen';
+import { FontAwesome, Feather, MaterialIcons } from '@expo/vector-icons'; 
+
 
 const Tab = createBottomTabNavigator();
 
 export const AppNavigator = () => {
   return (
       <Tab.Navigator >
-        <Tab.Screen name='Home' component={HomeScreen} 
-          options={{
-          // tabBarStyle: { display: "none" },
-            headerShown:false,
-          }}
+        {/* <Icon.Tab.Screen name="microphone"></Icon.Tab.Screen> */}
+        <Tab.Screen name='Home' component={HomeScreen}
+              options={
+                {
+                headerShown:false,
+                tabBarLabel: () => null,
+                tabBarIcon: () => (
+                  <MaterialIcons name="login" size={24} color="black" />
+                ),
+            }} 
         />
-        <Tab.Screen name='Record' component={RecordScreen}/>
-        <Tab.Screen name='Rave' component={RaveScreen} />
+
+        <Tab.Screen name='Record' component={RecordScreen}
+              options={{
+                tabBarLabel: () => null,
+                tabBarIcon: () => (
+                  <FontAwesome name='microphone' size={24} color="black"/>
+                ),
+            }} 
+        />
+        <Tab.Screen name='Rave' component={RaveScreen} 
+              options={{
+                tabBarLabel: () => null,
+                tabBarIcon: () => (
+                  <Feather name="headphones" size={24} color="black" />
+                )
+              }}
+        />
       </Tab.Navigator>
   );
 }; 
