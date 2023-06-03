2efc7518f84ae02cc44d241639cb7535aa84b2bc
Youba.I <mevshypocrisie@yahoo.com>
2023-06-03 19:34:08 +0200

    🚧(work): rave screen in construction

--- screens/RaveScreen.js
+++ screens/RaveScreen.js
@@ -1,12 +1,47 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet, Image } from "react-native";
 
 const RaveScreen = () => {
-    return(
-        <View>
-            <Text> RaveScreen </Text>
-        </View>
-    );
+  return (
+    <View style={styles.container}>
+      <View style={styles.titleContainer}>
+        <Image
+          style={styles.tinyLogo}
+          source={require("../assets/jsrave_logo.png")}
+        />
+        <Text style={styles.title}>Rave</Text>
+        <Text style={styles.label}>Stamp transfer</Text>
+      </View>
+    </View>
+  );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 40,
+    padding: 30,
+    backgroundColor: "#8BD0FC",
+  },
+  tinyLogo: {
+    width: 84,
+    height: 30,
+    alignSelf: "flex-start",
+    marginTop: 40,
+    marginBottom: 67,
+  },
+  titleContainer: {
+    marginBottom: 45,
+  },
+  title: {
+    fontSize: 47,
+    fontWeight: "900",
+    fontFamily: "Montserrat-Bold",
+  },
+  label: {
+    fontSize: 16,
+    fontFamily: "Montserrat-Regular",
+  },
+});
 
 export default RaveScreen;
